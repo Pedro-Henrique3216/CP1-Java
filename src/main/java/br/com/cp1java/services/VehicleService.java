64f498cebaae34660137e02d53476c6dfa8f73de
Pedro-Henrique3216 <ph6704938@gmail.com>
2025-03-31 11:19:34 -0300

    feat: create vehicle service

---
+++ src/main/java/br/com/cp1java/services/VehicleService.java
@@ -0,0 +1,15 @@
+package br.com.cp1java.services;
+
+import br.com.cp1java.repositories.VehicleRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VehicleService {
+
+    private final VehicleRepository vehicleRepository;
+
+    public VehicleService(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
+}
