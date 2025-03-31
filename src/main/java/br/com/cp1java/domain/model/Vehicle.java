c028d16e9f47637f9edb36bf1f6261efea365bec
Pedro-Henrique3216 <ph6704938@gmail.com>
2025-03-31 20:03:46 -0300

    feat: add JPA annotations to Vehicle entity and gets

--- src/main/java/br/com/cp1java/domain/model/Vehicle.java
+++ src/main/java/br/com/cp1java/domain/model/Vehicle.java
@@ -1,5 +1,6 @@
 package br.com.cp1java.domain.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -10,15 +11,26 @@ import java.util.UUID;
 @Getter
 @Setter
 @EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "Vehicles")
 public class Vehicle {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @Column(nullable = false)
     private String marca;
+    @Column(nullable = false)
     private String modelo;
+    @Column(nullable = false)
     private Integer ano;
+    @Column(nullable = false)
     private Double potencia;
+    @Column(nullable = false)
     private Double economia;
+    @Column(nullable = false)
     private VehicleType tipo;
+    @Column(nullable = false)
     private BigDecimal preco;
 
     public Vehicle(String marca, String modelo, Integer ano, Double potencia, Double economia, VehicleType tipo, BigDecimal preco) {
@@ -29,5 +41,37 @@ public class Vehicle {
         this.economia = economia;
         this.tipo = tipo;
         this.preco = preco;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public Double getPotencia() {
+        return potencia;
+    }
+
+    public Double getEconomia() {
+        return economia;
+    }
+
+    public VehicleType getTipo() {
+        return tipo;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
     }
 }
