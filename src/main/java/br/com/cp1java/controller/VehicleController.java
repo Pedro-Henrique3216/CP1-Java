e19b3d287bebabcb8cfe697e842af0f7825efc34
Pedro-Henrique3216 <ph6704938@gmail.com>
2025-03-31 11:25:24 -0300

    fix: add @RestController and @RequestMapping annotations

--- src/main/java/br/com/cp1java/controller/VehicleController.java
+++ src/main/java/br/com/cp1java/controller/VehicleController.java
@@ -1,7 +1,11 @@
 package br.com.cp1java.controller;
 
 import br.com.cp1java.services.VehicleService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/carros")
 public class VehicleController {
 
     private final VehicleService vehicleService;
