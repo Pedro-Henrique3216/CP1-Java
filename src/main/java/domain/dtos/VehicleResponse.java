76839995beff01002d355688a02d50457083f68c
Pedro-Henrique3216 <ph6704938@gmail.com>
2025-03-31 11:57:48 -0300

    refactor: add field id

--- src/main/java/domain/dtos/VehicleResponse.java
+++ src/main/java/domain/dtos/VehicleResponse.java
@@ -3,8 +3,10 @@ package domain.dtos;
 import domain.model.VehicleType;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 public record VehicleResponse(
+        UUID id,
         String marca,
         String modelo,
         Integer ano,
