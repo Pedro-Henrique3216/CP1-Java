a302f13b367e0229a86aa81825109d3f14f56eaa
Pedro-Henrique3216 <ph6704938@gmail.com>
2025-03-31 10:43:35 -0300

    first commit

---
+++ src/main/java/br/com/cp1java/Cp1JavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cp1java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp1JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cp1JavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/cp1java/Cp1JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cp1java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cp1JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
